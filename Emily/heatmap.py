83f1d973abb2ce3be9f50717fe407fc50d069d56
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2024-09-25 18:54:34 -0400

    Update heatmap.py

--- Emily/heatmap.py
+++ Emily/heatmap.py
@@ -3,7 +3,6 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Load the differential expression results from the CSV file
 de_results = pd.read_csv('de_results_optimized.csv')
 
 df = pd.read_csv("SRP158491_converted.tsv", sep='\t')
@@ -11,21 +10,18 @@ df = pd.read_csv("SRP158491_converted.tsv", sep='\t')
 df = df.dropna(subset=['Gene'])
 df = df.drop_duplicates(subset='Gene', keep='first')
 
-# Load patient metadata
 patient_df = pd.read_csv("SRP158491/metadata_SRP158491.tsv", sep='\t')
 
-# Select healthy and unhealthy samples based on metadata
+# healthy vs unhealthy samples based on metadata
 healthy_rows = patient_df[patient_df['refinebio_disease'] == 'healthy']
 unhealthy_rows = patient_df[patient_df['refinebio_disease'] == 'ra non treatment']
 
 healthy_columns = healthy_rows['refinebio_accession_code'].tolist()
 unhealthy_columns = unhealthy_rows['refinebio_accession_code'].tolist()
 
-# Subset the gene expression data for healthy and unhealthy samples
 healthy_gene_expression = df[['Gene'] + healthy_columns]
 unhealthy_gene_expression = df[['Gene'] + unhealthy_columns]
 
-# Step 1: Filter for significant genes
 significant_genes = de_results[(de_results['PValue'] < 0.05) & (abs(de_results['Log2FoldChange']) > 0.5)]
 print(f"Number of significantly differentially expressed genes: {len(significant_genes)}")
 
@@ -34,7 +30,6 @@ significant_gene_list = significant_genes['Gene'].tolist()
 significant_healthy_expression = healthy_gene_expression[healthy_gene_expression['Gene'].isin(significant_gene_list)]
 significant_unhealthy_expression = unhealthy_gene_expression[unhealthy_gene_expression['Gene'].isin(significant_gene_list)]
 
-# Combine the healthy and unhealthy expression data for heatmap
 combined_expression = pd.concat([significant_healthy_expression.set_index('Gene'), 
                                  significant_unhealthy_expression.set_index('Gene')], axis=1)
 
